c3417f15d4fcdda77bc397290487b7e7b261b3ce
De1t4 <mauriciochambicaceres@gmail.com>
2023-10-10 18:53:07 -0300

    fixed grid

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -59,7 +59,7 @@ export default function Proyectos() {
     <>
 			<main className={` border-black w-auto h-auto p-4 dark:bg-zinc-900`}>
 				<h1 className={`text-center mb-4 text-4xl font-semibold dark:bg-zinc-900 text-[#485caa] dark:border-zinc-500`}>Proyectos</h1>
-				<div className={`grid grid-rows-4 border-t-2 grid-flow-col gap-4 w-[50rem] m-auto max-lg:grid-rows-6 max-lg:w-auto dark:bg-zinc-900 animate-fade-in`}>
+				<div className={`grid grid-cols-2 gap-4 w-[50rem] m-auto max-md:grid-cols-1 max-md:w-auto dark:bg-zinc-900 animate-fade-in`}>
 					{proyectos.map(({nombre, descripcion, imagen, pagina, repositorio})=>(
 						<React.Fragment key={nombre}>
 							<ProyectosProps key={nombre} nombre={nombre} descripcion={descripcion} imagen={imagen} pagina={pagina} repositorio={repositorio}></ProyectosProps>
