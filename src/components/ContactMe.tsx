d1c06782b3b71873fdb32d224e37619616d6bc8b
De1t4 <mauriciochambicaceres@gmail.com>
2025-08-29 14:40:57 -0300

    add function email

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,11 +1,32 @@
-import { type FormEvent } from "react";
+import { type FormEvent, useRef } from "react";
+import emailjs from '@emailjs/browser';
+import { toast } from "sonner";
 
 export default function ContactMe() {
 
 
-  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const form = useRef<HTMLFormElement>(null);
+  const OnSendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm('email_service_22', 'template_8eahz7k', form.current, {
+          publicKey: 'qUwMOO_DufbL6nI8f',
+        })
+        .then(
+          () => {
+            toast.success("¡Gracias por tu mensaje!. Te estaré respondiendo a la brevedad.");
+            (e.target as HTMLFormElement).reset();
+          },
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          (_error) => {
+            toast.error("Ocurrió un inconveniente. Por favor, inténtalo nuevamente más tarde.");
+          },
+        );
+    }
   };
+
   return (
     <section id="contact" className="py-16 px-4 my-4 bg-gray-900/50">
       <div className="container mx-auto max-w-2xl">
@@ -15,7 +36,7 @@ export default function ContactMe() {
         </p>
         <div className="bg-gray-800 border-gray-700 rounded-md">
           <div className="p-6">
-            <form onSubmit={handleFormSubmit} className="space-y-6">
+            <form ref={form} onSubmit={OnSendEmail} className="space-y-6">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium mb-2 text-gray-300">
                   Email
@@ -60,6 +81,7 @@ export default function ContactMe() {
               </div>
               <button
                 type="submit"
+                value={"Send"}
                 className="w-full p-2 rounded-md font-semibold bg-emerald-500 hover:bg-emerald-600 text-gray-950"
               >
                 Enviar Mensaje
@@ -72,3 +94,4 @@ export default function ContactMe() {
 
   )
 }
+
